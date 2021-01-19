00000ea5a284c8ffb69ac661576d3d2b24eb9f89
Kotopoulos <76406336+Kotopoulos@users.noreply.github.com>
2021-01-19 15:24:08 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -2,14 +2,14 @@ module.exports = {
     name: 'poll',
     description: "Poll!",
     execute (message, args, Discord){
-    const Embed = new.RichEmbed()
+    const newEmbed = new Discord.MessageEmbed()
             .setColor('#9e7d03')
             .setTitle('Inititate Poll')
             .setAuthor('Community Rules', 'https://cdn.discordapp.com/attachments/800059670277783623/801014163706478622/Unreal4.png')
             
 
             if(!args[1]){
-                message.channel.send(Embed)                
+                message.channel.send(Embed);                
             }
         break;
      }
